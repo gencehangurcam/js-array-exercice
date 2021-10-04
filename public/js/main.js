c6c870285e6c6b44fc95e5ad1dfbc6d254b76af3
Gencehan <gencehangurcam@gmail.com>
2021-10-04 10:45:36 +0200

    pro init

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+// let ar = ['genc', 'gweny', 'wassime', 'ayhan'];
+// console.log(ar);
+// console.log(ar[3]);
+// console.log(ar[0].length);
+
+// let ar1 = ['genc']
+// console.log(ar1);
+// ar1.push('genc')
+// console.log(ar1);
+
+let coding = []
+coding.push('ayhan')
+coding.push('benedicte')
+coding.push('louise', 'jamila')
+coding.push('sebastian', 'adam', 'nico')
+coding.push('dawid', 'malo');
+coding.push('alex');
+coding.splice(coding.indexOf('alex'),1);
+coding.push('maxebce', 'mohamed');
+coding.push('saliou');
+coding.push('zulma', 'maxilm');
+coding.push('natchez');
+console.log(coding);    
